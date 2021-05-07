00ab1799be69dd341c9c37705e6212b1c30088e8
Matt Puchyr <mattpuchyr@gmail.com>
2021-05-07 15:15:26 -0500

    Create getPokemon function in API/API.tsx

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -1,6 +1,21 @@
+const totalPokemon: number = 151;
+
 export interface IPokemon {
     id: number;
     name: string;
     image: string;
-    type: string[];
+    types: string[];
+}
+
+const getPokemon = async (id: number): Promise<IPokemon> => {
+    const data: Response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    const pokemon: any = await data.json()
+    const pokemonToReturn: IPokemon = {
+        id: pokemon.id,
+        name: pokemon.name,
+        image: `${pokemon.sprites.front_default}`,
+        types: pokemon.types
+    }
+
+    return pokemonToReturn
 }
